820b7681316b77b539086051e554e770a63f32f4
Alexandre Henrique da Rocha Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2022-07-04 17:36:32 -0300

    lib emvia email ok

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const routes = require('./src/routes');
 const cors = require('cors');
-const bodyParser = require('body-parser');
-
 
 const app = express();
 
-// app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use(routes)
-
 
 app.listen(process.env.PORT || 3045, function () {
     console.log('🚀🚀🤖 servidor em execução')
