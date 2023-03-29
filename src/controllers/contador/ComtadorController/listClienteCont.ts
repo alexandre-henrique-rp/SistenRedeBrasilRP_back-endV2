ac2e88a5228c2e5fda3ef25761831c2ca105511f
Alexandre Henrique da Rocha Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2023-03-29 20:39:55 -0300

    updata rota de produção

--- src/controllers/contador/ComtadorController/listClienteCont.ts
+++ src/controllers/contador/ComtadorController/listClienteCont.ts
@@ -3,22 +3,41 @@ import { Request, Response } from 'express';
 import { Fcweb } from '../../../database/models/fcweb';
 
 export const ContListClient = async (req: Request, res: Response) => {
+  const hoje = new Date(); // data atual
+  const mesPassado = new Date(hoje.getFullYear(), hoje.getMonth() - 1, 1); // mês anterior com dia 1
+  mesPassado.setDate(1); // altera o dia para o primeiro dia do mês passado
 
   await Fcweb.findAll({
-    attributes: ['id', 'contador', 'tipocd', 'dt_aprovacao', 'comissaoparceiro', 'andamento', 'scp'],
+    attributes: [
+      'id',
+      'contador',
+      'tipocd',
+      'dt_aprovacao',
+      'comissaoparceiro',
+      'andamento',
+      'scp',
+      'id_fcw_soluti',
+      'estatos_pgto',
+    ],
     order: [['dt_aprovacao', 'DESC']],
     where: {
       contador: {
         [Op.substring]: req.params.contador,
       },
-      scp: "A PAGAR",
-      andamento:{
-        [Op.or]: ['EMITIDO','APROVADO']
-      }
+      dt_aprovacao: {
+        [Op.gte]: mesPassado,
+      },
+      estatos_pgto: 'Pago',
+      scp: {
+        [Op.ne]: 'PAGO',
+      },
+      id_fcw_soluti: {
+        [Op.ne]: [''],
+      },
     },
   })
     .then((cliente: any) => {
-      console.log(cliente.length)
+      console.log(cliente);
       res.status(200).json(cliente);
     })
     .catch((err: any) => {
